6291384376818dee7deb5d3797076f0a6fb3c077
Tom Durrant <tomchdurrant@gmail.com>
2023-12-08 13:18:19 +0000

    Complete the primes

--- primes.py
+++ primes.py
@@ -2,5 +2,15 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+    if number_of_primes <= 0:
+        raise ValueError
+    output = []
+    i = 2
+    while len(output) < number_of_primes:
+        for j in range(2, i):
+            if i % j == 0:
+                break
+        else:
+            output.append(i)
+        i += 1
+    return output
